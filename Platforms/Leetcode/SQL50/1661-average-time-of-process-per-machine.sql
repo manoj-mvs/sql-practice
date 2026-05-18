d5bc57da683fb477182fc2d9ea2e5acee44a53e8
Manoj <manojvsmaddula@gmail.com>
2026-05-18 21:58:11 +0530

    Create 1661-average-time-of-process-per-machine.sql

---
+++ Platforms/Leetcode/SQL50/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,22 @@
+-- LeetCode 1661
+-- Average Time of Process per Machine
+-- Difficulty: Easy
+
+/*
+Problem:
+Calculate the average time each machine takes to complete a process.
+
+Concepts Used:
+- SELF JOIN
+- AVG()
+- GROUP BY
+- ROUND()
+- Table Aliasing
+*/
+
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM Activity a1
+JOIN Activity a2
+ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
+WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
